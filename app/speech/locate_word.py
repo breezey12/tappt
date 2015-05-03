1e3a691bb2a039bd3a7fff942c0481e2b5e9aed3
Tom Hadley <tmh.2790@gmail.com>
2015-05-03 13:30:22 -0400

    properly skips download/transcription steps if json exists

--- app/speech/locate_word.py
+++ app/speech/locate_word.py
@@ -5,7 +5,7 @@ from download_yt import convert_yt_to_mp3
 
 def get_words(file_path):
     """returns list of words with attributes (time, confidence)"""
-    with open(file_path) as raw_json:    
+    with open(file_path, 'rw') as raw_json:    
         word_dict = json.load(raw_json)
     word_list = word_dict['words']
 
@@ -15,9 +15,9 @@ def get_words(file_path):
 def round_seconds(sec):
     """rounds and returns 5 secs earlier"""
     sec = int(float(sec))
-    if sec - 5 < 0:
+    if sec - 1 < 0:
         sec = 0
-    else: sec = sec - 5
+    else: sec = sec - 1
     return sec
 
 def locate_keywords(word_list, keywords):
@@ -32,14 +32,24 @@ def locate_keywords(word_list, keywords):
             print word['name'], word['time']
     return times
 
-def call_speechmatic_api(audio_file_path, api_token):
+def call_speechmatic_api(audio_file_path, api_token, unique_json_id):
     """creates json word_list and returns its path""" 
     #syntax: speechmatics.py -f example.mp3 -l en-US -i 1049 -t Y2E4OGM3NzItOGMwZi00OGQyLTk3MDctY2FmYWE3ZmYxZmNk -o example.json
-    c = "python speech/speechmatics.py -f " + audio_file_path + " -l en-US -i 1049 -t " + api_token + " -o speech/example.json"
+    c = "python speech/speechmatics.py -f " + audio_file_path + " -l en-US -i 1049 -t " + api_token + " -o " + unique_json_id 
     os.system(c)
-    return "speech/example.json"
+    return 
 
 def run_word_loc(url, api_token, keywords):
-    audio_file_path = convert_yt_to_mp3(url)
-    file_path = call_speechmatic_api(audio_file_path, api_token)
-    return locate_keywords(get_words(file_path), keywords)
+    unique_id = url[-11:].encode("utf-8")
+    unique_json_id = "speech/" + unique_id + ".json"
+    if unique_id + ".json" not in os.listdir('speech/'):
+        audio_file_path = convert_yt_to_mp3(url)
+        call_speechmatic_api(audio_file_path, api_token, unique_json_id)
+        clear_out_media_files()
+    return locate_keywords(get_words(unique_json_id), keywords)
+
+def clear_out_media_files():
+     for file_name in os.listdir('speech/media_files/'):
+         if file_name[-4:] == ".mp4" or file_name[-4:] == '.mp3':
+             full_path = 'speech/media_files/'
+             os.remove(os.path.join(full_path, file_name))
