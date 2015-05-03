4fd362fe3301497157585c16454efc49aaf493e7
Tom Hadley <tmh.2790@gmail.com>
2015-05-02 21:47:15 -0400

    embed is working

--- app/views.py
+++ app/views.py
@@ -17,11 +17,11 @@ def homepage():
         return render_template('index.html')
     if request.method == 'POST':
         youtube_url = request.form['youtube_url']
-        keyword = request.form['keyword']
-        #keyword = 'tolerate'
-        #times = locate_keywords(get_words('speech/example.json'), [keyword])
-        times = run_word_loc(youtube_url, app.config['SPEECHMATICS_API_KEY'], [keyword]) 
-        stream_urls = [youtube_url + "&t=" + str(time_location+5) + "s" for time_location in times]
+        #keyword = request.form['keyword']
+        keyword = 'tolerate'
+        times = locate_keywords(get_words('speech/example.json'), [keyword])
+        #times = run_word_loc(youtube_url, app.config['SPEECHMATICS_API_KEY'], [keyword])
+        youtube_urls = [youtube_url.replace('watch?v=', 'embed/') + "?start=" + str(time_loc + 5) for time_loc in times]
         return render_template('youtube_embed.html', 
-            stream_urls=stream_urls,
+            youtube_urls=youtube_urls,
             keyword=keyword)
